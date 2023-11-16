5edcfc94ded81c71a682a8564a2492bf383c7cda
Faiem-qy <f.quanyoung@gmail.com>
2023-11-16 00:40:36 -0500

    added code to show component with out classes

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,12 +2,17 @@ import React, { Component } from "react";
 
 import classnames from "classnames";
 
-class Dashboard extends Component {
-  render() {
-    const dashboardClasses = classnames("dashboard");
+// class Dashboard extends Component {
+//   render() {
+//     const dashboardClasses = classnames("dashboard");
 
-    return <main className={dashboardClasses} />;
-  }
+//     return <main className={dashboardClasses} />;
+//   }
+// }
+
+function Dashboard(props) {
+  const dashboardClasses = classnames("dashboard");
+  return <main className={dashboardClasses} />;
 }
 
 export default Dashboard;
